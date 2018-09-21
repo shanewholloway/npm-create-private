7cd9ba4b8881d8b66a583922da2c701b3912f64d
Shane Holloway <shane.holloway@ieee.org>
2018-09-20 18:05:41 -0600

    Fixed a few small initial-rollout bugs

--- create-private.js
+++ create-private.js
@@ -1,3 +1,5 @@
+#!node
+
 const fs = require('fs')
 
 const pkg_default = {
@@ -13,13 +15,17 @@ function create_private(package_json_filename='package.json') {
     fs.readFile(package_json_filename, 'utf-8',
       (err, data) => err ? resolve() : resolve(data) ))
   .then(data => data ? JSON.parse(data) : null)
-  .then(existing => Object.assign(existing, pkg_default, existing))
+  .then(existing => Object.assign(existing || {}, pkg_default, existing))
   .then(package_json => {
     const json_src = JSON.stringify(package_json, null, 2)
-    console.dir(json_src)
     return new Promise((resolve, reject) =>
       fs.writeFile(package_json_filename, json_src,
         (err) => err ? reject(err) : resolve(package_json) ))
+  }).then(package_json => {
+    console.log()
+    console.log(`Wrote "${package_json_filename}" with:`)
+    console.dir(package_json)
+    console.log()
   })
 }
 
